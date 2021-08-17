0749520cacce1d9a7d9d3ce067be3e2a5de89529
Crusoe Birkhead <81332371+crusoebirkhead@users.noreply.github.com>
2021-08-16 18:46:40 -0700

    Add files via upload
    Git corrupted again, uploading commit via file upload. Figured out its improper shut down of VM. Restyled calc and basic functions now return values.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.getElementById('subtract').onclick = operand;
 document.getElementById('multiply').onclick = operand;
 document.getElementById('divide').onclick = operand;
 let operator = ''
+let output = document.querySelector('#output')
+
+//Operations
 function add (num1, num2) {
 	 return num1 + num2
 }
@@ -13,6 +16,11 @@ function subtract (num1, num2) {
 
 function multiply(num1, num2){
 	return num1 * num2
+}
+
+function divide (num1, num2){
+	return num1 / num2
+
 }
 function power(num1, num2) {
 	return num1 ** num2
@@ -29,30 +37,58 @@ function factorial(num) {
 	return answer
 }
 
+//Brings in operand from calculator
 function operand(){
 	operator = this.id;
-	console.log(operator)
+	num1 = parseInt(output.value, 10)
+	output.value = ''
 }
 
-function operate(a, b){
+//Calls on operand 
+document.querySelector('.key-equal')
+.addEventListener('click', event => {
+	num2 = Number(output.value)
+	console.log(num2)
+	if (operator === 'add'){
+		// return add(num1, num2)
+		output.value = add(num1, num2)
+	}
+	if (operator === 'subtract'){
+		// return subtract(num1, num2)
+		output.value = subtract(num1, num2)
+	}
+	if (operator === 'multiply'){
+		// return multiply(num1, num2)
+		output.value = multiply(num1, num2)
+	}
+	if (operator === 'divide'){
+		// return divide(num1, num2)
+		output.value = divide(num1, num2)
+	}
+})
+	
 
-}
-
+//Display numbers clicked in output
 document.querySelector('.keys')
-  .addEventListener('click', event => {
+.addEventListener('click', event => {
     let target = event.target;
-    if (target.matches('button')) {
+    if (target.matches('.key')) {
       let value = target.innerHTML
-      
+
       document.querySelector('#output').value += value
     }
   });
 
+  //Clear button function
   document.querySelector('.clear')
   .addEventListener('click', event => {
 	document.querySelector('#output').value = ''
 	operator = ''
 	num1 = ''
 	num2 = ''
-
   })
+
+
+
+
+
